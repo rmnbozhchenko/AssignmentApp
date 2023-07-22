ee42b8d3f1defe1696409a7ff1711991a7e417b3
Roman Bozhchenko <roman.bozhchenko@ocado.com>
2023-07-22 08:17:02 +0200

    Initial Commit

---
+++ AssignmentApp/AssignmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentAppApp.swift
+//  AssignmentApp
+//
+//  Created by Roman Bozhchenko on 22/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
